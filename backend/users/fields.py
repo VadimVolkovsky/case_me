275e62f51cd27142ff313eeda2a7037e800c9e1d
vadim <vadimbor93@mail.ru>
2023-02-01 11:54:16 +0300

    добавлены исключения try/except для загрузки аватарок

--- backend/users/fields.py
+++ backend/users/fields.py
@@ -7,8 +7,13 @@ from rest_framework import serializers
 class Base64ImageField(serializers.ImageField):
     """Добавление аватарок пользователей"""
     def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
+        try:
+            if isinstance(data, str) and data.startswith('data:image'):
+                format, imgstr = data.split(';base64,')
+                ext = format.split('/')[-1]
+                data = ContentFile(
+                    base64.b64decode(imgstr), name='temp.' + ext
+                )
+            return super().to_internal_value(data)
+        except ValueError:
+            raise ValueError('Недопустимый формат изображения')
