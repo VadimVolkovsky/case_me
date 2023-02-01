018b57e58fb4b9a083084041070882581b3137db
vadim <vadimbor93@mail.ru>
2023-02-01 12:45:08 +0300

    добавлена докстринга для валидатора username_me

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -2,6 +2,8 @@ from django.core import exceptions
 
 
 def username_me(value):
+    """Запрещает регистрацию пользователя с username 'me'
+    для избежания конфликтов с эндпоинтом "/users/me/" (djoser) """
     if value == 'me':
         raise exceptions.ValidationError(
             'Имя пользователя "me" запрещено. '
