962b36b6d03a4d4e96b21d4f78f627456b8fdfa1
Olga Pomytkina <olpoma@yandex.ru>
2023-02-13 13:28:32 +0300

    feat: checkbox react template

---
+++ frontend/registration/Reg.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Checkbox from './Checkbox.js';
+import './Reg.css';
+
+function Reg() {
+  return (
+    <div className="Reg">
+      <Checkbox />
+      </div>
+  );
+}
+
+export default Reg;
+
+
+
