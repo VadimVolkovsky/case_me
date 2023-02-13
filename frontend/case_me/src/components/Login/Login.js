9700c941d21fa2181c68a156ed00feb7c165f654
MarinaPrivalova <privalovama@yandex.ru>
2023-02-13 17:56:14 +0300

    style, fix: input_lables

--- frontend/case_me/src/components/Login/Login.js
+++ frontend/case_me/src/components/Login/Login.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Link, useLocation} from "react-router-dom";
+import "./Login.css";
 
 function Login() {
   return (
-    <div className="Login">
-      <h1> Страница авторизации CaseMe</h1>
+    <main className="content">
+    <div className="authorize">
+      <div className="authorize__title-links">
+        <Link className="authorize__title-link">Вход</Link>
+        <Link className="authorize__title-link">Регистрация</Link>
+      </div>
+      <form className="form-authorize" novalidate>
+        <fieldset className="form-authorize__set">
+          <div className="form-authorize__field">
+            <label className="form-authorize__input-label" for="email">E-mail</label>
+            <input className="form-authorize__input form-authorize__input_type_email" placeholder="Введите e-mail" id="email" type="email" name="email" required/>
+            <span className="form-authorize__input-error email-error"></span>
+          </div>
+          <div className="form-authorize__field">
+            <label className="form-authorize__input-label" for="password">Пароль</label>
+            <input className="form-authorize__input form-authorize__input_type_password" placeholder="Введите пароль" id="password" type="password" name="password" required/>
+            <span className="form-authorize__input-error password-error"></span>
+          </div>
+          <button className="form-authorize__enter-btn" type="submit">Войти</button>
+        </fieldset>
+      </form>
+      <Link className="authorize__link">Забыли пароль?</Link>
     </div>
+
+  </main>
   );
 }
 
