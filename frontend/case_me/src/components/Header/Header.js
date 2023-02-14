e018dc59ebbd16aa4cc8e4ba7c43e1ad62aa642f
MarinaPrivalova <privalovama@yandex.ru>
2023-02-14 08:24:04 +0300

    feat: header, fix: activ liks login, style

--- frontend/case_me/src/components/Header/Header.js
+++ frontend/case_me/src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../img/logo_case_me.svg";
 
-function Header(){
-return(
-  <header className="header">
-
-  </header>
-)
+function Header() {
+  return (
+    <header class="header">
+      <img class="header__logo" src={logo} alt="Логотип"/>
+    </header>
+  )
 }
 
 export default Header;
--- frontend/case_me/src/components/Login/Login.js
+++ frontend/case_me/src/components/Login/Login.js
@@ -1,34 +1,35 @@
 import React from "react";
-import { Link, useLocation} from "react-router-dom";
+import { Link, NavLink} from "react-router-dom";
 import "./Login.css";
+
+const setActive = ({ isActive }) => (isActive ? "authorize__title-link_active" : "authorize__title-link");
 
 function Login() {
   return (
     <main className="content">
-    <div className="authorize">
-      <div className="authorize__title-links">
-        <Link className="authorize__title-link">Вход</Link>
-        <Link className="authorize__title-link">Регистрация</Link>
+      <div className="authorize">
+        <div className="authorize__title-links">
+          <NavLink to="/signin" className={setActive}>Вход</NavLink>
+          <NavLink to="/signup" className={setActive}>Регистрация</NavLink>
+        </div>
+        <form className="form-authorize" novalidate>
+          <fieldset className="form-authorize__set">
+            <div className="form-authorize__field">
+              <label className="form-authorize__input-label" for="email">E-mail</label>
+              <input className="form-authorize__input form-authorize__input_type_email" placeholder="Введите e-mail" id="email" type="email" name="email" required />
+              <span className="form-authorize__input-error email-error"></span>
+            </div>
+            <div className="form-authorize__field">
+              <label className="form-authorize__input-label" for="password">Пароль</label>
+              <input className="form-authorize__input form-authorize__input_type_password" placeholder="Введите пароль" id="password" type="password" name="password" required />
+              <span className="form-authorize__input-error password-error"></span>
+            </div>
+            <button className="form-authorize__enter-btn" type="submit">Войти</button>
+          </fieldset>
+        </form>
+        <Link to="/passwordform" className="authorize__link">Забыли пароль?</Link>
       </div>
-      <form className="form-authorize" novalidate>
-        <fieldset className="form-authorize__set">
-          <div className="form-authorize__field">
-            <label className="form-authorize__input-label" for="email">E-mail</label>
-            <input className="form-authorize__input form-authorize__input_type_email" placeholder="Введите e-mail" id="email" type="email" name="email" required/>
-            <span className="form-authorize__input-error email-error"></span>
-          </div>
-          <div className="form-authorize__field">
-            <label className="form-authorize__input-label" for="password">Пароль</label>
-            <input className="form-authorize__input form-authorize__input_type_password" placeholder="Введите пароль" id="password" type="password" name="password" required/>
-            <span className="form-authorize__input-error password-error"></span>
-          </div>
-          <button className="form-authorize__enter-btn" type="submit">Войти</button>
-        </fieldset>
-      </form>
-      <Link className="authorize__link">Забыли пароль?</Link>
-    </div>
-
-  </main>
+    </main>
   );
 }
 
