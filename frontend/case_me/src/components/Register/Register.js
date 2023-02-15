930c8d268e0d0138858899898b12026eff8488a4
Olga Pomytkina <olpoma@yandex.ru>
2023-02-16 02:50:36 +0300

    feat: валидация формы регистарции

--- frontend/case_me/src/components/Register/Register.js
+++ frontend/case_me/src/components/Register/Register.js
@@ -1,14 +1,90 @@
 import React from "react";
 import Checkbox from '../Checkbox/Checkbox';
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import './Register.css';
-
 
 
 function Register() {
 
-  const location = useLocation();
-  const activeLink = (path) => location.pathname === path ? 'registration__link_active' : '';
+  const activeLink = ({ isActive }) => (isActive) ? 'registration__link_active' : 'registration__link';
+
+  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [nicknameDirty, setNicknameDirty] = useState(false);
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+
+  const [nicknameError, setNicknameError] = useState('Поле не может быть пустым');
+  const [emailError, setEmailError] = useState('Поле не может быть пустым');
+  const [passwordError, setPasswordError] = useState('Поле не может быть пустым');
+
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (nicknameError || emailError || passwordError) {
+      setFormValid(false)
+    } else {
+      setFormValid(true)
+    }
+  }, [nicknameError, emailError, passwordError])
+
+  const nicknameHandler = (e) => {
+    setNickname(e.target.value);
+    const re = /^@[a-zA-Z0-9_.-]{2,19}$/;
+    if (!re.test(`@${e.target.value}`)) {
+      setNicknameError('Только латиница (a-z), цифры (0-9), символы (_-.@), не меньше 3 и не больше 20 символов');
+      if (!e.target.value) {
+        setNicknameError('Поле не может быть пустым');
+      }
+    } else {
+      setNicknameError('');
+      setNickname(`@${e.target.value}`);
+    }
+  }
+
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError('Введите корректный e-mail');
+      if (!e.target.value) {
+        setEmailError('Поле не может быть пустым');
+      }
+    } else {
+      setEmailError('');
+    }
+  }
+
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 8 || e.target.value.length > 50) {
+      setPasswordError('Длина пароля не может быть меньше 8 и больше 50 символов');
+      if (!e.target.value) {
+        setPasswordError('Поле не может быть пустым');
+      }
+    } else {
+      setPasswordError('');
+    }
+  }
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'nickname':
+        setNicknameDirty(true);
+        break;
+      case 'email':
+        setEmailDirty(true);
+        break;
+      case 'password':
+        setPasswordDirty(true);
+        break;
+    }
+  }
 
   return (
     <div>
@@ -17,38 +93,73 @@ function Register() {
           <section className="registration__info">
             <nav>
               <ul className="registration__links">
-                <Link className="registration__link-decor" to="/signin"><a href="/signin" className={`registration__link ${activeLink('/sigin')}`}>Вход</a></Link>
-                <Link className="registration__link-decor" to="/signup"><a href="/signup" className={`registration__link ${activeLink('/signup')}`}>Регистрация</a></Link>
+                <NavLink to="/signin" className={activeLink}>Вход</NavLink>
+                <NavLink to="/signup" className={activeLink}>Регистрация</NavLink>
               </ul>
             </nav>
 
-            <form action="/" className="registration__form" novalidate>
-              <label className="registration__form-label" for="name">Никнейм</label>
-              <input className="registration__input registration__input_type_name" id="name" type="text" name="name" placeholder="@" required />
-                <span className="registration__input-error name-error"></span>
+            <form action="/" novalidate>
+              <div className="registration__input-container">
+                <label className="registration__form-label" for="nickname">Никнейм</label>
 
-                <label className="registration__form-label" for="email">E-mail</label>
-                <input className="registration__input registration__input_type_email" id="email" type="email" name="email" required />
-                  <span className="registration__input-error email-error"></span>
-
-                  <label className="registration__form-label" for="password">Пароль</label>
-                  <input className="registration__input registration__input_type_password" placeholder="Не менее 8 символов" id="password" type="password" name="password" minlength="8" required />
-                    <span className="registration__input-error password-error"></span>
-
-                    <div className="registration__checkbox-form">
-                      <Checkbox id="checkbox" name="checkbox" required={true} />
-                      <p class="registration__policy-text">Я согласен с <Link class="registration__policy-link" href="#">Политикой конфиденциальности</Link> и <Link class="registration__policy-link" href="#">Пользовательским соглашением</Link></p>
-                    </div>
-
-                    <Link className="registration__link-decor" to="/info"><a href="/info" class="registration__submit-button registration__link-decor" type="submit" disabled="true">Зарегистрироваться</a></Link>
-                  </form>
-                </section>
+                <input
+                  onChange={e => {nicknameHandler(e);}}
+                  value={nickname}
+                  onBlur={e => blurHandler(e)}
+                  className={`registration__input ${nicknameDirty && nicknameError ? 'registration__input_type_error' : ''}`}
+                  id="nickname"
+                  type="text"
+                  name="nickname"
+                  placeholder="@" required />
+                {(nicknameDirty && nicknameError) && <span className="registration__input-error nickname-error">{nicknameError}</span>}
               </div>
 
-            </main>
+              <div className="registration__input-container">
+                <label className="registration__form-label" for="email">E-mail</label>
+                <input
+                  onChange={e => emailHandler(e)}
+                  value={email}
+                  onBlur={e => blurHandler(e)}
+                  className={`registration__input ${emailDirty && emailError ? 'registration__input_type_error' : ''}`}
+                  id="email"
+                  type="email"
+                  name="email"
+                  required />
+                {(emailDirty && emailError) && <span className="registration__input-error email-error">{emailError}</span>}
+              </div>
 
+              <div className="registration__input-container">
+                <label className="registration__form-label" for="password">Пароль</label>
+                <input
+                  onChange={e => passwordHandler(e)}
+                  value={password}
+                  onBlur={e => blurHandler(e)}
+                  className={`registration__input ${passwordDirty && passwordError ? 'registration__input_type_error' : ''}`}
+                  placeholder="Не менее 8 символов"
+                  id="password"
+                  type="password"
+                  name="password"
+                  required />
+                {(passwordDirty && passwordError) && <span className="registration__input-error password-error">{passwordError}</span>}
+              </div>
+
+              <div className="registration__checkbox-form">
+                <Checkbox id="checkbox" name="checkbox" required={true} />
+                <p class="registration__policy-text">Я согласен с <Link class="registration__policy-link" to="">Политикой конфиденциальности</Link> и <Link class="registration__policy-link" to="">Пользовательским соглашением</Link></p>
+              </div>
+
+              <button
+                type="submit"
+                className={`registration__submit-button ${formValid ? "registration__submit-button_active" : "form-authorize__enter-btn_disabled"}`}
+                disabled={!formValid}>Зарегистрироваться</button>
+            </form>
+          </section>
         </div>
-        );
+
+      </main>
+
+    </div>
+  );
 }
 
-        export default Register;
+export default Register;
