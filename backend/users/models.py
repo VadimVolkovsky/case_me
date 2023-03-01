574fda2af7d0c3eed44af9a2fdd6c7403e18f72d
vadim <vadimbor93@mail.ru>
2023-03-01 17:51:49 +0300

    добавлено поле is_active для восстановления пароля

--- backend/users/models.py
+++ backend/users/models.py
@@ -141,7 +141,7 @@ class User(AbstractBaseUser):
         blank=True,
         verbose_name='Twitter'
     )
-
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
