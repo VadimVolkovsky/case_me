fc5b84675776464a83b44d5c9d0e89eb49141f23
vadim <vadimbor93@mail.ru>
2023-04-01 18:46:45 +0300

    test corse

--- backend/case_me/settings.py
+++ backend/case_me/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'drf_yasg',
     'djoser',
     "phonenumber_field",
@@ -35,6 +36,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -140,3 +142,5 @@ SIMPLE_JWT = {
 USERNAME_MIN_LENGTH = 3
 USERNAME_MAX_LENGTH = 20
 USERNAME_REGEX = r'[a-zA-Z0-9]{3,20}'
+
+CORS_ORIGIN_ALLOW_ALL = True
