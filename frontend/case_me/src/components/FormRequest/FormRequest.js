3f751e07471b45cf3a001ddfac4c16cf444f5e8e
Maria Gromyko <mazvereva91@mail.ru>
2023-04-05 13:03:57 +0300

    feat: add gh-pages

--- frontend/case_me/src/components/FormRequest/FormRequest.js
+++ frontend/case_me/src/components/FormRequest/FormRequest.js
@@ -4,21 +4,20 @@ import ErrorNotification from "../ErrorNotification/ErrorNotification";
 
 function FormRequest(props) {
 
-const {name, title, textSubmit, children, formValid, onSubmit} = props
+  const { name, title, textSubmit, children, formValid, onSubmit } = props
 
   return (
-    <>
 
-    <div className={`form form_type_${name}`}>
-    <ErrorNotification />
-      <form className="form__content" onSubmit={onSubmit} noValidate>
-      <h2 className="form__title">{title}</h2>
-     {children}
-        <button type="submit" disabled={!formValid} className={`form__button ${formValid ? "form__button_active" : "form__button_disabled"}`}>{textSubmit}</button>
-      </form>
-
+    <div className="FormRequest">
+      <div className={`form form_type_${name}`}>
+        <ErrorNotification />
+        <form className="form__content" onSubmit={onSubmit} noValidate>
+          <h2 className="form__title">{title}</h2>
+          {children}
+          <button type="submit" disabled={!formValid} className={`form__button ${formValid ? "form__button_active" : "form__button_disabled"}`}>{textSubmit}</button>
+        </form>
+      </div>
     </div>
-    </>
   );
 }
 
